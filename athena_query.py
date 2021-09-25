d91f5d5827b9d76b12bd128ac7a9901727db05a7
singh0021 <singh0021@hotmail.com>
2021-09-25 19:19:48 +0100

    For lambda development

---
+++ athena_query.py
@@ -0,0 +1,110 @@
+import boto3, sys, json, time, uuid, datetime, botocore, re
+
+import traceback
+
+
+def my_print(msg):
+    print("{} {}".format(datetime.datetime.utcnow(), msg))
+
+
+class AthenaMetrics(object):
+    region_name = 'us-east-1'
+    client = boto3.session.Session(region_name=region_name).client('athena')
+
+    s3_folder = None
+
+    def __init__(self, s3_folder):
+
+        self.s3_folder = s3_folder
+
+    def execute_save_s3(self, sql_query, s3_output_folder):
+
+        QueryExecutionId = self.start_query_execution(sql_query=sql_query, s3_output_folder=s3_output_folder)
+        result_status_result = self.__wait_for_query_to_complete(QueryExecutionId)
+        if result_status_result["SUCCESS"]:
+            return True
+        else:
+            raise Exception(result_status_result)
+
+    def execute_query(self, sql_query, use_cache=False):
+
+        QueryExecutionId = self.start_query_execution(sql_query=sql_query, use_cache=use_cache)
+        return self.get_results(QueryExecutionId=QueryExecutionId)
+
+    def start_query_execution(self, sql_query, use_cache=False, s3_output_folder=None):
+
+        outputLocation = 's3://' + self.s3_folder if s3_output_folder is None else s3_output_folder
+
+        response = self.client.start_query_execution(
+            QueryString=sql_query,
+            ClientRequestToken=str(uuid.uuid4()) if not use_cache else sql_query[:64].ljust(32) + str(hash(sql_query)),
+            ResultConfiguration={
+                'OutputLocation': outputLocation,
+            }
+        )
+        return response["QueryExecutionId"]
+
+    def get_results(self, QueryExecutionId):
+
+        result_status_result = self.__wait_for_query_to_complete(QueryExecutionId)
+
+        result = None
+        if result_status_result["SUCCESS"]:
+            paginator = self.client.get_paginator('get_query_results')
+            page_response = paginator.paginate(QueryExecutionId=QueryExecutionId)
+            # PageResponse Holds 1000 objects at a time and will continue to repeat in chunks of 1000.
+
+            for page_object in page_response:
+
+                if result is None:
+                    result = page_object
+                    if result_status_result["QUERY_TYPE"] == "SELECT":
+                        if len(result["ResultSet"]["Rows"]) > 0:
+                            # removes column header from 1st row (Athena returns 1st row as col header)
+                            del result["ResultSet"]["Rows"][0]
+                else:
+                    result["ResultSet"]["Rows"].extend(page_object["ResultSet"]["Rows"])
+            result["ResponseMetadata"]["HTTPHeaders"]["content-length"] = None
+            return result
+        else:
+            raise Exception(result_status_result)
+
+    def __wait_for_query_to_complete(self, QueryExecutionId):
+
+        status = "QUEUED"  # assumed
+        error_count = 0
+        response = None
+        while (status in ("QUEUED','RUNNING")):  # can be QUEUED | RUNNING | SUCCEEDED | FAILED | CANCELLED
+            try:
+                response = self.client.get_query_execution(QueryExecutionId=QueryExecutionId)
+                status = response["QueryExecution"]["Status"]["State"]
+                # my_print(status)
+                time.sleep(0.5)
+            except botocore.exceptions.ClientError as ce:
+
+                error_count = error_count + 1
+                if (error_count > 3):
+                    status = "FAILED"
+                    print(str(ce))
+                    break  # out of the loop
+                if "ExpiredTokenException" in str(ce):
+                    self.client = boto3.session.Session(region_name=self.region_name).client('athena')
+
+        if (status == "FAILED" or status == "CANCELLED"):
+            # print(response)
+            pass
+
+        if response is None:
+            return {"SUCCESS": False,
+                    "STATUS": status
+                , "QUERY_TYPE": response["QueryExecution"]["Query"].strip().upper().split(" ")[0]
+                , "QUERY": response["QueryExecution"]["Query"]
+                , "StateChangeReason": response["QueryExecution"]["Status"][
+                    "StateChangeReason"] if "StateChangeReason" in response["QueryExecution"]["Status"] else None}
+        else:
+            return {"SUCCESS": True if response["QueryExecution"]["Status"]["State"] == "SUCCEEDED" else False,
+                    "STATUS": response["QueryExecution"]["Status"]["State"]
+                , "QUERY_TYPE": response["QueryExecution"]["Query"].strip().upper().split(" ")[0]
+                , "QUERY": response["QueryExecution"]["Query"]
+                , "StateChangeReason": response["QueryExecution"]["Status"][
+                    "StateChangeReason"] if "StateChangeReason" in response["QueryExecution"]["Status"] else None}
---
+++ main.py
@@ -0,0 +1,114 @@
+from athena_query import AthenaMetrics
+import boto3
+import datetime
+import sys
+import os
+import csv, time
+
+
+def array_chunk(array, chunksize):
+    res = []
+    lower = 0
+    for j in range(lower, len(array)):
+        tmp = []
+        upper = lower + chunksize
+        if (upper >= len(array)):
+            upper = len(array)
+        for k in range(lower, upper):
+            tmp.append(array[k])
+        lower = upper
+        res.append(tmp)
+        if (upper >= len(array)):
+            return res
+
+
+def lambda_handler(event, context):
+    folder = os.environ['folder_path']
+    bucket = os.environ['bucket']
+
+    bucket_folder = bucket + '/' + folder
+
+    client = boto3.client('athena', region_name='us-east-1')
+
+    ## The Athena query on cloudtrails table to get last days' query IDs
+    query_str = """
+                with data as
+                (SELECT json_extract(responseelements, '$.queryExecutionId') as query_id,
+                (useridentity.arn) as uid,
+                (useridentity.sessioncontext.sessionIssuer.userName) as role,
+                from_iso8601_timestamp(eventtime) as dt
+                FROM "cloudtrail"."cloudtrail_logs_nm"
+                WHERE eventsource='athena.amazonaws.com'
+                AND eventname='StartQueryExecution'
+                AND useragent!='quicksight.amazonaws.com'
+                AND json_extract(responseelements, '$.queryExecutionId') is not null)
+                SELECT * FROM data WHERE dt >  date_add('day',-1,now())
+                """
+
+    result = AthenaMetrics(s3_folder=bucket_folder) \
+        .execute_query(query_str)
+
+    query_ids = []
+    for row in result["ResultSet"]["Rows"]:
+        data = row['Data'][0]
+        print(row)
+        item = data['VarCharValue'].strip('"')
+        query_ids.append(item)
+
+    allqueryids = (query_ids[1:])
+
+    j = 0
+
+    for batchqueryids in array_chunk(allqueryids, 50):
+        try:
+            response = client.batch_get_query_execution(
+                QueryExecutionIds=batchqueryids
+            )
+            athena_metrics = ""
+            for row in response["QueryExecutions"]:
+                queryid = row['QueryExecutionId']
+                querydatabase = "null"
+                if 'QueryExecutionContext' in row and 'Database' in row['QueryExecutionContext']:
+                    querydatabase = row['QueryExecutionContext']['Database']
+                executiontime = "null"
+                if 'EngineExecutionTimeInMillis' in row['Statistics']:
+                    executiontime = str(row['Statistics']['EngineExecutionTimeInMillis'])
+                datascanned = "null"
+                if 'DataScannedInBytes' in row['Statistics']:
+                    datascanned = str(row['Statistics']['DataScannedInBytes'])
+                status = row['Status']['State']
+                submissiondatetime = "null"
+                if 'SubmissionDateTime' in row['Status']:
+                    submissiondatetime = str(row['Status']['SubmissionDateTime'])
+                completiondatetime = "null"
+                if 'CompletionDateTime' in row['Status']:
+                    completiondatetime = str(row['Status']['CompletionDateTime'])
+                athena_metrics += ','.join(
+                    [queryid, querydatabase, executiontime, datascanned, status, submissiondatetime,
+                     completiondatetime]) + '\n'
+        except Exception as e:
+            print(e)
+
+        filename = "query_result_" + str(j) + ".csv"
+        lambda_path = "/tmp/" + filename
+
+        f = open(lambda_path, 'w')
+        f.write(athena_metrics)
+        f.close()
+
+        s3 = boto3.resource('s3', region_name='us-east-1')
+
+        now = datetime.datetime.now()
+        current_year = now.year
+        current_month = f"{now.month:02d}"
+        current_day = f"{now.day :02d}"
+
+        outfile = 'athena-metrics' + '/' + str(current_year) + '/' + str(current_month) + '/' + str(
+            current_day) + '/' + 'query_result_' + str(j) + '.csv'
+        s3.meta.client.upload_file(lambda_path, bucket, outfile)
+
+        j = j + 1
+
+        print("Printing Athena Metrics on Console", athena_metrics)
+
+
